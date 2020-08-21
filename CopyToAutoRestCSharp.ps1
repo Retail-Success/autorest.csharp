ebaace54bbf81b76999f87e5bede90f14e1559c6
colby-rs <colbys@retailsuccess.com>
2020-08-21 10:16:40 -0500

    csharp extension 2.3.84 upgrade attempt

--- CopyToAutoRestCSharp.ps1
+++ CopyToAutoRestCSharp.ps1
@@ -1,34 +1,34 @@
-if (Test-Path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\azure") {
-	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\azure" -recurse
+if (Test-Path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\azure") {
+	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\azure" -recurse
 }
-if (Test-Path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\azurefluent") {
-	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\azurefluent" -recurse
+if (Test-Path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\azurefluent") {
+	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\azurefluent" -recurse
 }
-if (Test-Path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\jsonrpc") {
-	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\jsonrpc" -recurse
+if (Test-Path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\jsonrpc") {
+	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\jsonrpc" -recurse
 }
-if (Test-Path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\simplifier") {
-	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\simplifier" -recurse
+if (Test-Path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\simplifier") {
+	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\simplifier" -recurse
 }
-if (Test-Path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\vanilla") {
-	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\vanilla" -recurse
+if (Test-Path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\vanilla") {
+	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\vanilla" -recurse
 }
 
-Copy-Item -Path "src\azure" -Destination "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\azure" -Recurse
-Copy-Item -Path "src\azurefluent" -Destination "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\azurefluent" -Recurse
-Copy-Item -Path "src\jsonrpc" -Destination "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\jsonrpc" -Recurse
-Copy-Item -Path "src\simplifier" -Destination "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\simplifier" -Recurse
-Copy-Item -Path "src\vanilla" -Destination "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\vanilla" -Recurse
+Copy-Item -Path "src\azure" -Destination "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\azure" -Recurse
+Copy-Item -Path "src\azurefluent" -Destination "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\azurefluent" -Recurse
+Copy-Item -Path "src\jsonrpc" -Destination "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\jsonrpc" -Recurse
+Copy-Item -Path "src\simplifier" -Destination "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\simplifier" -Recurse
+Copy-Item -Path "src\vanilla" -Destination "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\vanilla" -Recurse
 
 if (Test-Path "published") {
 	Remove-Item -path "published" -recurse
 }
 dotnet publish -o published
-if (Test-Path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\bin\netcoreapp2.0") {
-	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\bin\netcoreapp2.0\*" -recurse
+if (Test-Path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\bin\netcoreapp2.0") {
+	Remove-Item -path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\bin\netcoreapp2.0\*" -recurse
 }
-If (!(Test-Path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\bin\netcoreapp2.0\")) {
-    New-Item -Path "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\bin\netcoreapp2.0\" -ItemType Directory
+If (!(Test-Path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\bin\netcoreapp2.0\")) {
+    New-Item -Path "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\bin\netcoreapp2.0\" -ItemType Directory
 }
 
-Copy-Item -Path "published\*" -Destination "@microsoft.azure_autorest.csharp@2.3.82\node_modules\@microsoft.azure\autorest.csharp\src\bin\netcoreapp2.0\" -Recurse
+Copy-Item -Path "published\*" -Destination "@microsoft.azure_autorest.csharp@2.3.84\node_modules\@microsoft.azure\autorest.csharp\src\bin\netcoreapp2.0\" -Recurse
--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
 FROM azuresdk/autorest
-COPY ./@microsoft.azure_autorest.csharp@2.3.82 /root/.autorest/@microsoft.azure_autorest.csharp@2.3.82
+COPY ./@microsoft.azure_autorest.csharp@2.3.84 /root/.autorest/@microsoft.azure_autorest.csharp@2.3.84
